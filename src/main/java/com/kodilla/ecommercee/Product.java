51fca092d3bfaeb34d0cdd45b27d2c85471d2b3c
qub3rt <hubert.krzeslak@gmail.com>
2020-04-30 23:18:08 +0200

    impementacja zadania 200402-12

--- src/main/java/com/kodilla/ecommercee/Product.java
+++ src/main/java/com/kodilla/ecommercee/Product.java
@@ -1,4 +1,53 @@
 package com.kodilla.ecommercee;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+    private String productName;
+    private double productWeight;
+    private double productPrice;
+    private String productType;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductWeight() {
+        return productWeight;
+    }
+
+    public void setProductWeight(double productWeight) {
+        this.productWeight = productWeight;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
 }
