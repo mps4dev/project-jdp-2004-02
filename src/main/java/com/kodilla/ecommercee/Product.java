c48244fb6ab048bfaf9ea368b1868410a014cadc
qub3rt <hubert.krzeslak@gmail.com>
2020-05-01 15:36:45 +0200

    wstępna implementacja zadania 12

--- src/main/java/com/kodilla/ecommercee/Product.java
+++ src/main/java/com/kodilla/ecommercee/Product.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
     private String productName;
     private double productWeight;
