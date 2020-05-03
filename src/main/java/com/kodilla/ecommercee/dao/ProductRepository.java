bc3796f18263487cd2c77033defc3e050337342a
qub3rt <hubert.krzeslak@gmail.com>
2020-05-03 14:02:16 +0200

    JDP200402-12 Encja produkt - lombok, uporządkowanie pakietów

--- src/main/java/com/kodilla/ecommercee/dao/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/dao/ProductRepository.java
@@ -1,5 +1,6 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.dao;
 
+import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,25 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private double productWeight;
+    private double productPrice;
+    private String productType;
+}
+
+
+
