649c9ca79d481f73fd84eaec8edbc7ba7f0f3911
silverltheone <adeksiwy@gmail.com>
2020-05-04 16:10:21 +0200

    CartController - wstępna implementacja endpointów

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,41 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.ProductDto;
+import com.kodilla.ecommercee.model.CartDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/cart")
 public class CartController {
+    List<ProductDto> products = new ArrayList<>();
+
+    @PutMapping
+    public void create(@RequestBody CartDto cartDto) {
+
+    }
+
+    @GetMapping("/{productId}")
+    public ProductDto getProduct(@PathVariable long productId) {
+        return new ProductDto();
+    }
+
+    @PostMapping("/{productId}")
+    public void addProduct(@PathVariable long productId) {
+
+    }
+
+    @DeleteMapping("/{productId}")
+    public void deleteProduct(@PathVariable long productId) {
+
+    }
+
+    @PutMapping
+    public void makeOrder(@RequestBody CartDto cartDto) {
+
+    }
 }
+
+
