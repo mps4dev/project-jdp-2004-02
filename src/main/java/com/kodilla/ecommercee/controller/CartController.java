6d2a4ca79bafeec75d4c6eb5ef7bffd11c4a0f9c
silverltheone <adeksiwy@gmail.com>
2020-05-04 16:10:21 +0200

    CartController - wstępna implementacja endpointów

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,7 +1,6 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.dto.CartDto;
-import com.kodilla.ecommercee.dto.OrderDto;
+import com.kodilla.ecommercee.model.CartDto;
 import com.kodilla.ecommercee.dto.ProductDto;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,35 +10,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/cart")
 public class CartController {
+    List<ProductDto> products = new ArrayList<>();
 
     @PutMapping
     public void create(@RequestBody CartDto cartDto) {
 
     }
 
-    @GetMapping("/{cartId}")
-    public ProductDto getProduct(@RequestParam long productId, @PathVariable long cartId) {
+    @GetMapping("/{productId}")
+    public ProductDto getProduct(@PathVariable long productId) {
         return new ProductDto();
     }
 
-    @GetMapping
-    public List<ProductDto> getAllProducts() {
-    return new ArrayList<>();
-    }
-
-    @PostMapping("/{cartId}")
-    public void addProduct(@RequestParam long productId, @PathVariable long cartId) {
+    @PostMapping("/{productId}")
+    public void addProduct(@PathVariable long productId) {
 
     }
 
-    @DeleteMapping("/{cartId}")
-    public void deleteProduct(@RequestParam long productId, @PathVariable long cartId) {
+    @DeleteMapping("/{productId}")
+    public void deleteProduct(@PathVariable long productId) {
 
     }
 
-    @PutMapping("makeOrder")
-    public OrderDto makeOrder(@RequestBody CartDto cartDto) {
-        return new OrderDto();
+    @PutMapping
+    public void makeOrder(@RequestBody CartDto cartDto) {
+
     }
 }
 
