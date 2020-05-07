a96345c3d1c68f0e02a019b759dbd8d5f310661d
silverltheone <adeksiwy@gmail.com>
2020-05-07 21:45:42 +0200

    JDP200402-3  CartController - wstępna implementacja endpointów. Zmiany po rewiev. v.5

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,7 +1,8 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.model.CartDto;
-import com.kodilla.ecommercee.model.ProductDto;
+import com.kodilla.ecommercee.dto.CartDto;
+import com.kodilla.ecommercee.dto.OrderDto;
+import com.kodilla.ecommercee.dto.ProductDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,31 +11,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/cart")
 public class CartController {
-    List<ProductDto> products = new ArrayList<>();
 
-    @PutMapping
+    @PostMapping
     public void create(@RequestBody CartDto cartDto) {
 
     }
 
-    @GetMapping("/{productId}")
-    public ProductDto getProduct(@PathVariable long productId) {
+    @GetMapping("/{cartId}")
+    public ProductDto getProduct(@RequestParam long productId, @PathVariable long cartId) {
         return new ProductDto();
     }
 
-    @PostMapping("/{productId}")
-    public void addProduct(@PathVariable long productId) {
+    @GetMapping("/{cartId}/products")
+    public List<ProductDto> getAllProducts(@PathVariable long cartId) {
+    return new ArrayList<>();
+    }
+
+    @PostMapping("/{cartId}")
+    public void addProduct(@RequestParam long productId, @PathVariable long cartId) {
 
     }
 
-    @DeleteMapping("/{productId}")
-    public void deleteProduct(@PathVariable long productId) {
+    @DeleteMapping("/{cartId}")
+    public void deleteProduct(@RequestParam long productId, @PathVariable long cartId) {
 
     }
 
-    @PutMapping
-    public void makeOrder(@RequestBody CartDto cartDto) {
-
+    @PutMapping("/makeOrder")
+    public OrderDto makeOrder(@RequestBody CartDto cartDto) {
+        return new OrderDto();
     }
 }
 
