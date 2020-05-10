98d6872c77e070a01e44cffaa0dd890878e86a54
qub3rt <hubert.krzeslak@gmail.com>
2020-05-10 23:43:49 +0200

    JDP200402-12 Encja Product - dodanie relacji z encjami Cart i Group

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,24 +1,42 @@
 package com.kodilla.ecommercee.domain;
 
+import com.kodilla.ecommercee.GenericEntity;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-@Entity
+@Entity(name = "PRODUCT")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
+@Data
+public class Product extends GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long productId;
+    @Column(name = "PRODUCT_ID")
+    private long productId;
+    @NotBlank
+    @Column(name = "PRODUCT_NAME")
     private String productName;
-    private double productWeight;
+    @NotNull
+    @Column(name = "PRICE")
     private double productPrice;
-    private String productType;
+
+    // relacja z encją group
+//    @ManyToOne
+//    @JoinColumn(name = "GROUP_ID")
+//    private Group groupList;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "JOIN_PRODUCTS_CARTS",
+            inverseJoinColumns = {@JoinColumn(name = "CART_ID", referencedColumnName = "ID")},
+            joinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "ID")}
+    )
+    private List<Cart> carts;
 }
 
 
