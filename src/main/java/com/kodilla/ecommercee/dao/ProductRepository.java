c7dc9c0accfc33da4e9ab77f45dcb49b2b4e7b0d
qub3rt <64071532+qub3rt@users.noreply.github.com>
2020-05-13 23:13:14 +0200

    Delete ProductRepository.java

--- src/main/java/com/kodilla/ecommercee/dao/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.ecommercee.dao;
-
-import com.kodilla.ecommercee.GenericEntityRepository;
-import com.kodilla.ecommercee.domain.Product;
-
-public interface ProductRepository extends GenericEntityRepository<Product> {
-}
