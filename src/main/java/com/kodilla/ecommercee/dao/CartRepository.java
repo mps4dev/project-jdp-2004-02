f9b13b4c62c1b907184eaf35d7cb388679b89e01
qub3rt <64071532+qub3rt@users.noreply.github.com>
2020-05-13 23:13:05 +0200

    Delete CartRepository.java

--- src/main/java/com/kodilla/ecommercee/dao/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.ecommercee.dao;
-
-import com.kodilla.ecommercee.GenericEntityRepository;
-import com.kodilla.ecommercee.domain.Cart;
-
-public interface CartRepository extends GenericEntityRepository<Cart> {
-}
