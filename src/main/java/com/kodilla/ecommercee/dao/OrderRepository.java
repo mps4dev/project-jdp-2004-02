19a4ec4db9983566639c038bf4fcc887113e0c7c
qub3rt <64071532+qub3rt@users.noreply.github.com>
2020-05-13 22:37:35 +0200

    Delete OrderRepository.java

--- src/main/java/com/kodilla/ecommercee/dao/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.ecommercee.dao;
-
-import com.kodilla.ecommercee.GenericEntityRepository;
-import com.kodilla.ecommercee.domain.Order;
-
-public interface OrderRepository extends GenericEntityRepository<Order> {
-}
