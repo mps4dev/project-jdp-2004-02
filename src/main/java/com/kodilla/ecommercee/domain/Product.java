8895e8c973d2344ed26030533d1ee44ebde9020a
qub3rt <hubert.krzeslak@gmail.com>
2020-05-18 00:47:43 +0200

    JDP200402-12 Encja Product - implementacja wg ostatniego diagramu

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,36 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity(name = "PRODUCTS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product extends GenericEntity {
+
+    @NotBlank
+    @Column(name = "PRODUCT_NAME")
+    private String productName;
+
+    @NotNull
+    @Column(name = "PRICE")
+    private double productPrice;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "GROUP_ID")
+    private Group group;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "CART_ID")
+    private Cart cart;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+}
---
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends GenericEntityRepository {
+}
