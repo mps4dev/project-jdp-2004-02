c129e1b8857d167613209a390deb8ea71d3ac18d
qub3rt <hubert.krzeslak@gmail.com>
2020-05-19 12:02:44 +0200

    JDP200402-11 Encja Order - implementacja zgodnie z zaakceptowanym diagramem

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,27 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "ORDERS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order extends GenericEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "order",
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY
+    )
+    private List<Product> products;
+}
