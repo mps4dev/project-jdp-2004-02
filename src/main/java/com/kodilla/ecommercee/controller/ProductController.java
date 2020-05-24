54c1a59d741cc2188d8bf691e7a33ab9d4eee567
silverltheone <adeksiwy@gmail.com>
2020-05-24 13:17:25 +0200

    ProductController - rzeczywista implementacja metod

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,37 +1,41 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.dto.ProductDto;
+import com.kodilla.ecommercee.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/product")
 public class ProductController {
 
+    @Autowired
+    ProductService productService;
+
     @GetMapping
     public List<ProductDto> getAll() {
-        return new ArrayList<>();
+        return productService.getAll();
     }
 
     @GetMapping("/{productId}")
-    public ProductDto get(@PathVariable long productId) {
-        return new ProductDto ();
+    public ProductDto get(@PathVariable Long productId) {
+        return productService.get(productId);
     }
 
     @PostMapping
     public ProductDto create(@RequestBody ProductDto productDto) {
-        return new ProductDto();
+        return productService.create(productDto);
     }
 
     @PutMapping
-    public ProductDto update(@RequestBody ProductDto product) {
-        return new ProductDto();
+    public ProductDto update(@RequestBody ProductDto productDto) {
+        return productService.update(productDto);
     }
 
     @DeleteMapping("/{productId}")
-    public boolean delete(@PathVariable long productId) {
-        return true;
+    public boolean delete(@PathVariable Long productId) {
+        return productService.delete(productId);
     }
 }
